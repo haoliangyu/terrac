943783b50d06ef013f7be22a04fd0f2206aefde6
Haoliang Yu <haoliang.yu@outlook.com>
2023-01-03 09:23:47 -0500

    set up the project

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-const chai = require("chai");
-const expect = chai.expect;
-
-describe("tests", () => {
-  it("should work", () => {
-    expect(true).to.equal(true);
-  });
-});
---
+++ test/index.test.ts
@@ -0,0 +1,4 @@
+test("Dummy unit test", () => {
+  const actual = null; // not implemented yet
+  expect(actual).toBe(1);
+});
