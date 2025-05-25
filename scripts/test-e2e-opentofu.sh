52a9aa0ccbc4e1c5bef712f508620b36f296a843
Haoliang Yu <hyu@esri.com>
2025-05-25 09:52:12 -0400

    fix e2e script

--- scripts/test-e2e-opentofu.sh
+++ scripts/test-e2e-opentofu.sh
@@ -6,7 +6,7 @@ WORK_DIR=test/fixtures/e2e-module-s3
 aws s3 rm s3://$TEST_BUCKET/ --recursive --exclude "*" --include "$TEST_MODULE*"
 
 # publish test module
-npm run start -- publish --work-directory $WORK_DIR ----overwrite-config backend.bucket=$TEST_BUCKET
+npm run start -- publish --work-directory $WORK_DIR --overwrite-config backend.bucket=$TEST_BUCKET
 
 # install test module
 cd $WORK_DIR && tofu init
